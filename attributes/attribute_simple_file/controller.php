eac03da08aae396988cde116a7ffeb71682a94be
Remo Laubacher <remo.laubacher@mesch.ch>
2018-01-04 09:04:08 +0100

    allow local files to be moved as well, we need this in multi step forms since we can't store $_FILES in a session

--- attributes/attribute_simple_file/controller.php
+++ attributes/attribute_simple_file/controller.php
@@ -20,6 +20,7 @@ class Controller extends AttributeTypeController
         $db = Database::connection();
         $db->executeQuery('DELETE from atSimpleFile where avID = ?', array($this->getAttributeValueID()));
     }
+
     /**
      * Returns the value entered in the HTML editor
      * @return string
@@ -79,18 +80,26 @@ class Controller extends AttributeTypeController
 
         $tmpName = $_FILES['akID']['tmp_name'][$akID]['value'];
         $name = $_FILES['akID']['name'][$akID]['value'];
-        if (!empty($tmpName) && is_uploaded_file($tmpName)) {
-            $directory = DIR_BASE . '/application/files/simple_file_attribute';
-            if (!file_exists($directory)) {
-                mkdir($directory);
-            }
-            $uniqueKey = uniqid();
-            $filename = $uniqueKey . '-' . $name;
 
-            if (move_uploaded_file($tmpName, $directory . '/' . $filename)) {
-                $db = Database::connection();
-                $db->Replace('atSimpleFile', array('avID' => $this->getAttributeValueID(), 'fileName' => $name, 'uniqueKey' => $uniqueKey), 'avID', true);
+        $directory = DIR_BASE . '/application/files/simple_file_attribute';
+        if (!file_exists($directory)) {
+            mkdir($directory);
+        }
+        $uniqueKey = uniqid();
+        $filename = $uniqueKey . '-' . $name;
+        $ok = false;
+
+        if (!empty($tmpName) && is_uploaded_file($tmpName)) {
+            $ok = move_uploaded_file($tmpName, $directory . '/' . $filename);
+        } else {
+            if (file_exists($tmpName)) {
+                $ok = rename($tmpName, $directory . '/' . $filename);
             }
+        }
+
+        if ($ok) {
+            $db = Database::connection();
+            $db->Replace('atSimpleFile', array('avID' => $this->getAttributeValueID(), 'fileName' => $name, 'uniqueKey' => $uniqueKey), 'avID', true);
         }
     }
 
